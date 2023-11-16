8bb6728b8e36cfe8510dfca65aedbb876ecb3ae2
main <guvanch.d.1999@gmail.com>
2023-11-16 11:30:39 +0500

    test

--- controllers/Controller.js
+++ controllers/Controller.js
@@ -2,7 +2,9 @@ const statusCodes = require('../utils/statusCodes')
 const service = require('../services/service')
 module.exports = {
     async preHook(request, response) {
-        console.log(request.body)
+        const data = request.body
+
+        console.log(data.payload)
 
         return response.status(200).json(request.body)
     }
