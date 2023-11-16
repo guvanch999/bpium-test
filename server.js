a598435790845580f8934a710c8b1a583e6d6bd5
main <guvanch.d.1999@gmail.com>
2023-11-16 11:21:13 +0500

    port change

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app = require("./app");
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 8474;
 const server = app.listen(PORT, "0.0.0.0", async () => {
     console.log(`Server started and listening on port ${PORT}...`);
 })
